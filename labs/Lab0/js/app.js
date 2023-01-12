f75a74ed6541468443a6959157bbabe88d1a9baa
stwayb <stwayb@gmail.com>
2023-01-12 11:40:36 -0500

    Creating lab 0

---
+++ labs/Lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(1200,768);
+    background(95, 49, 212);
+
+    fill(100,55,120);
+    rect( 40,50,100,20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 5);
+}
