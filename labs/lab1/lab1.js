d075436bd35dd807c6f55520c5f0f3b7b959d3b0
stwayb <stwayb@gmail.com>
2023-01-17 12:54:43 -0500

    Update lab1.js

--- labs/lab1/lab1.js
+++ labs/lab1/lab1.js
@@ -1,6 +1,5 @@
 function setup() {
     createCanvas(800,600);
-    background(255,100,255);
     let x =500;
     rect(x,0,30,100);
 }
@@ -9,6 +8,7 @@ let r=1;
 let t=0;
 let y=0;
 let z=0;
+let j=0;
 function draw() {
     y++;
 
@@ -25,18 +25,35 @@ function draw() {
         t=0;
     }
 
-    if(y==60){
+    if(y==60&&t==0){
         y=0;
         z++;
     }
 
-    if((z%2)==0){
+    if(y==60&&t==1){
+        y=0;
+        j++;
+    }
+
+    if((z%2)==0&&t==0){
         background(255,100,255);
     }
 
-    if((z%2)==1){
+    if((z%2)==1&&t==0){
         background(100,255,255);
     }
+    
+
+    if((j%2)==0&&t==1){
+        background(255,100,100);
+    }
+
+    if((j%2)==1&&t==1){
+        background(100,100,255);
+    }
+    
+
+
     rect(r,10,20,20)
 }
 
