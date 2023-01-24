3e2f31f2950021bec3d50e3d399085526ed78ad6
stwayb <stwayb@gmail.com>
2023-01-24 11:12:35 -0500

    algorithm check polar points

---
+++ labs/lab3/pp.js
@@ -0,0 +1,15 @@
+// create all variables needed
+
+// setup funtion to create the canvas
+
+// polarPoint(r) funciton with the srgument r
+
+    // creates an x and y variable and sets them to the sin and cos of the mouseX*r
+
+    // returns a vector at the new x,y position
+
+// draw function
+
+    // created the res object with the x and y values from the polarPoints function
+
+    // draw a circle at res coordinates 
