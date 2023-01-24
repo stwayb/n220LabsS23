7478ff7ed35de544d6edb179dc40fd7cab87dfa6
stwayb <stwayb@gmail.com>
2023-01-24 11:06:12 -0500

    Algorithm check red remover

---
+++ labs/lab3/rr.js
@@ -0,0 +1,13 @@
+//create the color with red in it
+
+// setup function to create the canvas
+
+// buid the noRed(color1) function that gets rid of the red value of a color
+
+// draw function
+
+// calls the noRed() function with the argument of color1
+
+// sets the fill to the new red-less color
+
+// draws a circle of that color
