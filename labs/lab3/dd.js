e45f076868ee6ba4e7ebf135a077bb7cfe4170d1
stwayb <stwayb@gmail.com>
2023-01-24 10:56:59 -0500

    Algorithm check for Drawing Drawers

---
+++ labs/lab3/dd.js
@@ -0,0 +1,11 @@
+//Create needed variables
+
+//Run setup function to create the canvas
+
+//Create buildCar(x,y) function to create the car at the given position
+
+//draw() function
+
+//Color the background to make the car not leave a trail
+
+//call the build car function with the arguments of whatever x andy position i want it to have
