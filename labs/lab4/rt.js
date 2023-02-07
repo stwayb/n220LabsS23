e93400df97e5ddd7295bdc7896bd363bb6791ff2
stwayb <stwayb@gmail.com>
2023-02-07 11:15:50 -0500

    reverse tennis finished

--- labs/lab4/rt.js
+++ labs/lab4/rt.js
@@ -1,31 +1,38 @@
 //Create needed variables
+const racL={
+    x:20,
+    y:90,
+    width:6,
+    height:20
+};
+const racR={
+    x:374,
+    y:90,
+    width:6,
+    height:20
+};
 
 //Run setup function to create the canvas
-
 function setup(){
-    createCanvas(800,600);
+    createCanvas(400,200);
 }
 
-//Create buildCar(x,y) function to create the car at the given position
 
-function buildCar(x,y){
-    fill(255,0,0);
-    rect(x,y+10,30,20,5,5,0,0);
-    fill(0,0,0);
-    circle(x,y+30,10);
-    circle(x+30,y+30,10);
-    square(x+20,y+10,10,0,5,0,0);
-}
 //draw() function
-
 function draw(){
+    if (keyIsDown(UP_ARROW)) {
+        racL.y+=2;
+        racR.y-=2;
+    }
+    
+    if (keyIsDown(DOWN_ARROW)) {
+        racL.y-=2;
+        racR.y+=2;
+    }
+
     //Color the background to make the car not leave a trail
     background(255);
-    //call the build car function with the arguments of whatever x andy position i want it to have
-    buildCar(mouseX,mouseY);
-    buildCar(mouseX+80,mouseY+300);
-    buildCar(mouseX-250,mouseY+210);
-    buildCar(mouseX-80,mouseY-10);
-    buildCar(mouseX+40,mouseY-90);
+    rect(racR.x,racR.y,racR.width,racR.height);
+    rect(racL.x,racL.y,racL.width,racL.height);
 }
 
