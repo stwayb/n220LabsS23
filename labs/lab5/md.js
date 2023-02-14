56ed8c2c182f6fb5674ca667bfdfb3dc4fb9875a
stwayb <stwayb@gmail.com>
2023-02-14 12:02:39 -0500

    Comments

--- labs/lab5/md.js
+++ labs/lab5/md.js
@@ -1,8 +1,13 @@
+//Keeps track of how many times youve clicked the text
 let count=0;
 
+//The function called by the onlcick
 function mcDiv(){
     count++;
+    //adds "mc" to the end of the text
     document.getElementById("md").innerHTML += "mc"
+
+    //if the count is divisable by 3 it changes the text to "divdivdiv"
     if(count%3==0){
         document.getElementById("md").innerHTML = "divdivdiv"
     }
