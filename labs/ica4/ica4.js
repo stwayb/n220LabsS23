93719f1634bc01956b3c0b6f3a665a2b374978cb
stwayb <stwayb@gmail.com>
2023-03-09 12:24:21 -0500

    ica4

--- labs/ica4/ica4.js
+++ labs/ica4/ica4.js
@@ -1,11 +1,5 @@
-function checkResponce(time){
-    if(time=="morning"){
-        document.getElementById("responce").innerHTML = "That is correct";
-    }
-    if(time=="afternoon"){
-        document.getElementById("responce").innerHTML = "Guess again";
-    }
-    if(time=="evening"){
-        document.getElementById("responce").innerHTML = "Guess again"; 
-    }
+let out=document.getElementById("out");
+
+function output(){
+    console.log(out.value+out.value);
 }
