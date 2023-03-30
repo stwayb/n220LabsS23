3f917718c894bebc66b80eb46ed7d72cdd9babfa
stwayb <stwayb@gmail.com>
2023-03-30 11:03:02 -0400

    vowel remover 2 function

--- labs/ica4/ica5.js
+++ labs/ica4/ica5.js
@@ -1,14 +1,29 @@
 let out=document.getElementById("out");
 let txt=document.getElementById("text");
 let vowels=["a","e","i","o","u","A","E","I","O","U"];
+function isVowel(char){
+    for(let y=0;y<vowels.length;y++){
+        if(char==vowels[y]){
+            console.log(char);
+            return true
+        }
+    }
+    return false
+}
+
+
 function noVowel(){
     let word=out.value;
     for(let x=0;x<word.length;x++){
-        for(let y=0;y<vowels.length;y++){
-            if(word[x]==vowels[y]){
-                word=word.replace(word.substring(x,1+x),"")
-            }
+        if(isVowel(word[x])){
+            console.log(word[x]);
+            word=word.replace(word.substring(x,1+x),"")
         }
     }
-    txt.innerHTML=word;
+    txt.innerHTML=word
 }
+
+
+
+
+//word=word.replace(word.substring(x,1+x),"")
